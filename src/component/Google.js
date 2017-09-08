4ef29b20c78da4bef6d165519da122d3d9fb0dca
gaowei <810915983@qq.com>
2017-09-08 17:03:16 +0800

    add google mark

---
+++ src/component/Google.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Administrator.
+ * Date at 2017/9/8.
+ */
+import React, {Component} from 'react';
+import './css/Google.css';
+export default class Google extends Component {
+    constructor(props){
+        super(props)
+    }
+    render() {
+        return (
+            <div style={{height:this.props.size,width:this.props.size}} className="Google">
+                <div className="Content">
+                    {
+                        this.props.visible?
+                        <div className="Mark">
+                            <div className="Red"></div>
+                            <div className="Green"></div>
+                            <div className="Yellow"></div>
+                            <div className="Blue"></div>
+                            <div className="WhiteLine"></div>
+                            <div className="White"></div>
+                            <div className="BlueLine"></div>
+                        </div>:null
+                    }
+                </div>
+            </div>
+        );
+    }
+}
