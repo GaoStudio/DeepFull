cb1f41c74d617fc07890374bdfa63f2dce41d5ba
gaowei <810915983@qq.com>
2017-09-08 17:33:11 +0800

    add Sidesbar

--- src/component/Phone.js
+++ src/component/Phone.js
@@ -1,22 +1,28 @@
 import React, {Component} from 'react';
-import './Phone.css';
-class App extends Component {
+import './css/Phone.css';
+import Google from './Google.js'
+export default class App extends Component {
     render() {
         return (
-        <div className="Phone">
-            <div className="Top">
-                <div className="TopTwo">
-                    <span className="Camera"></span>
-                    <span className="Receiver"></span>
+            <div className="Container">
+                <div className="Phone">
+                    <div className="Top">
+                        <div className="TopTwo">
+                            <span className="Camera"></span>
+                            <span className="Receiver"></span>
+                        </div>
+                        <span className="Sensor"></span>
+                        <span style={{bottom: 5}} className="Sidesbar"></span>
+                    </div>
+                    <div className="Screen">
+                    </div>
+                    <div className="Bottom">
+                        <Google visible={false} size={"8vh"}/>
+                        <span className="Sidesbar"></span>
+                    </div>
                 </div>
-                <span className="Sensor"></span>
             </div>
-            <div className="Screen">
-            </div>
-            <div className="Bottom">
-            </div>
-        </div>
         );
     }
 }
-export default App;
+
