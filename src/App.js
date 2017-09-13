749075d9752090e5302961c5afdb5a32bd72fedf
Jaye Gao <810915983@qq.com>
2017-09-13 23:46:44 +0800

    phone prefect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,76 @@
 import React, {Component} from 'react';
 import './App.css';
-import Phone from './component/Phone.js'
+
 class App extends Component {
+
+
+    componentDidMount(){
+       this._handleScroll();
+    }
+    componentWillUnmount(){
+
+    }
+    _handleScroll(toPage){
+        var body = document.body,
+            html = document.documentElement;
+        var itv, height = document.body.offsetHeight;
+        var page = scrollTop() / height | 0;
+        if(toPage){
+            page =toPage;
+            moveDown()
+        }else {
+            window.addEventListener("resize", onresize, false);
+            onresize();
+        }
+        document.body.addEventListener(
+            "onwheel" in document ? "wheel" : "mousewheel",
+            function(e) {
+                clearTimeout(itv);
+                itv = setTimeout(function() {
+                    var delta = e.wheelDelta / 120 || -e.deltaY / 3;
+                    page -= delta;
+                    var max = (document.body.scrollHeight / height | 0) - 1;
+                    if (page < 0) return page = 0;
+                    // if (page > max) return page = max;
+                    if(page==0){
+                        move();
+                    }else{
+                        moveDown();
+                    }
+
+                }, 100);
+                e.preventDefault();
+            }
+        );
+        function moveDown() {
+            var value = height *0.2* page;
+            var diff = scrollTop() - value;
+            (function callee() {
+                diff = diff / 1.2 | 0;
+                scrollTop(value + diff);
+                if (diff) itv = setTimeout(callee, 16);
+            })();
+        };
+        //ƽ۬ٶ֯
+        function move() {
+            var value = height * page;
+            var diff = scrollTop() - value;
+            (function callee() {
+                diff = diff / 1.2 | 0;
+                scrollTop(value + diff);
+                if (diff) itv = setTimeout(callee, 10);
+            })();
+        };
+        function onresize() {
+            height = body.offsetHeight;
+            move();
+        };
+        function scrollTop(v) {
+            if (v == null) return Math.max(body.scrollTop, html.scrollTop);
+            else body.scrollTop = html.scrollTop = v;
+        };
+    }
+
     constructor(props){
         super(props)
     }
@@ -11,15 +80,10 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="App-left">
-
+                <div className="HomeTop">
+                    <div className="DownAllow" onClick={()=>{this._handleScroll(1)}}></div>
                 </div>
-                <div className="App-center">
-                   {/*<Phone/>*/}
-                </div>
-                <div className="App-right">
-
-                </div>
+                <footer className="HomeBottom"></footer>
             </div>
         );
     }
