bbd31140d5d23b43b36f008452e0cc37194ef922
Jaye Gao <810915983@qq.com>
2017-09-17 23:24:30 +0800

    add post

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import PostPage from './page/post/PostPage.js'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<PostPage />, document.getElementById('root'));
 registerServiceWorker();
--- src/page/post/PostPage.js
+++ src/page/post/PostPage.js
@@ -3,7 +3,7 @@
  * Date at 2017/9/12.
  */
 import React, {Component} from 'react';
-import './css/PostPage.css';
+import './../Page.css';
 export default class PostPage extends Component {
     constructor(props) {
         super(props)
@@ -12,7 +12,20 @@ export default class PostPage extends Component {
     render() {
         return (
             <div className="PostPage">
-
+                <div className="PostContent">
+                    <div className="PostTop">
+                        <div className="PageControlTop">
+                            <img style={{width:40,height:40}} src={require("../../images/home.png")}></img>
+                            <img style={{width:40,height:40}} src={require("../../images/more.png")}></img>
+                        </div>
+                        <div className="PostPageTop">
+                            <img style={{width:100,height:100,borderRadius:50}}  src={require("../../images/head.gif")}></img>
+                            <text style={{color:"#363636",fontWeight: 600,fontSize:16,marginTop:10}}>Deep</text>
+                            <text style={{color:"#a9a9a9",fontSize:20,marginTop:10}}>梦想这东西和经典一样,永远不会因为时间而褪色，反而更加珍贵。</text>
+                        </div>
+                        <hr></hr>
+                    </div>
+                </div>
             </div>
         );
     }
