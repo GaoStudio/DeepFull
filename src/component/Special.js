83121b834bf16958d577cee652e1ece762d1d13d
Jaye Gao <810915983@qq.com>
2017-09-25 23:16:29 +0800

    add post

---
+++ src/component/Special.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './css/Special.css';
+export default class Special extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className="Special">
+                <img style={{width:75,height:75}} src={this.props.imagelogo}/>
+                <div className="SpecialContent">
+                    <text className="Title" >{this.props.title}</text>
+                    <div><text>文章</text><text  style={{fontWeight:"bold"}}> : </text><text>{this.props.pagenum}篇</text></div>
+                    <div><text>阅读</text><text  style={{fontWeight:"bold"}}> : </text><text>{this.props.readnum}</text></div>
+                </div>
+            </div>
+        );
+    }
+}
--- src/page/post/PostPage.js
+++ src/page/post/PostPage.js
@@ -5,28 +5,31 @@
 import React, {Component} from 'react';
 import './../Page.css';
 import Tag from "./../../component/Tag.js"
-import {Affix, Carousel} from 'antd';
+import Special from "./../../component/Special.js"
+import {Affix, Carousel, Card} from 'antd';
 import 'antd/dist/antd.css';
 export default class PostPage extends Component {
     constructor(props) {
         super(props)
     }
+
     render() {
         return (
             <div className="PostPage">
                 <div className="PostContent">
                     <div className="PostTop">
                         <div className="PostTopBg">
-                            <Carousel effect="fade" autoplay = {true} dots = {false}>
-                                <img src={require("../../images/bg_bg01.jpg")} style={{height:250}}></img>
-                                <img src={require("../../images/bg_bg02.jpg")} style={{height:250}}></img>
-                                <img src={require("../../images/dusk-bg.png")}  style={{height:250}}></img>
+                            <Carousel effect="fade" autoplay={true} dots={false}>
+                                <img src={require("../../images/bg_bg01.jpg")} style={{height: 250}}></img>
+                                <img src={require("../../images/bg_bg02.jpg")} style={{height: 250}}></img>
+                                <img src={require("../../images/dusk-bg.png")} style={{height: 250}}></img>
                             </Carousel>
                         </div>
                         <div className="PostTopContent">
                             <img style={{width: 100, height: 100, borderRadius: 50}}
                                  src={require("../../images/timg.jpg")}></img>
-                            <text style={{color: "#363636", fontWeight: 600, fontSize: 16, marginTop: 10}}>高卫的技术博客</text>
+                            <text style={{color: "#363636", fontWeight: 600, fontSize: 16, marginTop: 10}}>高卫的技术博客
+                            </text>
                         </div>
                         <div className="Sign">
                             <Affix style={{width: "100%"}}>
@@ -53,22 +56,42 @@ export default class PostPage extends Component {
                             left
                         </div>
                         <div className="PostSummary">
-                            <div className="PostSummaryMark">
-                                <Tag color="pink">Java</Tag>
-                                <Tag color="red">Android</Tag>
-                                <Tag color="orange">React</Tag>
-                                <Tag color="green">React Native</Tag>
-                                <Tag color="cyan">React Navigation</Tag>
-                                <Tag color="blue">RxJava</Tag>
-                                <Tag color="purple">线程</Tag>
-                                <Tag color="pink">pink</Tag>
-                                <Tag color="red">red</Tag>
-                                <Tag color="orange">orange</Tag>
-                                <Tag color="green">green</Tag>
-                                <Tag color="cyan">cyan</Tag>
-                                <Tag color="blue">blue</Tag>
-                                <Tag color="purple">purple</Tag>
-                            </div>
+                            <Card title="标签" bordered={true} bodyStyle={{ padding: 5 }} style={{marginTop: 20,marginRight:20}}>
+                                <div className="PostSummaryMark">
+                                    <Tag color="pink">Java</Tag>
+                                    <Tag color="red">Android</Tag>
+                                    <Tag color="orange">React</Tag>
+                                    <Tag color="green">React Native</Tag>
+                                    <Tag color="cyan">React Navigation</Tag>
+                                    <Tag color="blue">RxJava</Tag>
+                                    <Tag color="purple">线程</Tag>
+                                    <Tag color="pink">pink</Tag>
+                                    <Tag color="red">red</Tag>
+                                    <Tag color="orange">orange</Tag>
+                                    <Tag color="green">green</Tag>
+                                    <Tag color="cyan">cyan</Tag>
+                                    <Tag color="blue">blue</Tag>
+                                    <Tag color="purple">purple</Tag>
+                                </div>
+                            </Card>
+                            <Card title="专题" bordered={true} bodyStyle={{ padding: 5 }} style={{marginTop: 20,marginRight:20}}>
+                                <div className="PostSummaryMark">
+                                    <Special imagelogo={require("./../../images/react-nav-logo.svg")} title="React Navigation全解析" pagenum={20} readnum = {20} />
+                                    <div style={{backgroundColor:"#e8e8e8",height:1}}></div>
+                                    <Special imagelogo={require("./../../images/regengxin.png")} title="热更新" pagenum={20} readnum = {20} />
+                                    <div style={{backgroundColor:"#e8e8e8",height:1}}></div>
+                                    <Special imagelogo={require("./../../images/react-nav-logo.svg")} title="React Navigation全解析" pagenum={20} readnum = {20} />
+                                    <div style={{backgroundColor:"#e8e8e8",height:1}}></div>
+                                    <Special imagelogo={require("./../../images/regengxin.png")} title="热更新" pagenum={20} readnum = {20} />
+                                </div>
+                            </Card>
+                            <Card title="分类" bordered={true} bodyStyle={{ padding: 5 }} style={{marginTop: 20,marginRight:20}}>
+                                <div className="PostSummaryMark">
+                                    <Special imagelogo={require("./../../images/react-nav-logo.svg")} title="React Navigation全解析" pagenum={20} readnum = {20} />
+                                    <div style={{backgroundColor:"#e8e8e8",height:1}}></div>
+                                    <Special imagelogo={require("./../../images/regengxin.png")} title="热更新" pagenum={20} readnum = {20} />
+                                </div>
+                            </Card>
                         </div>
                     </div>
                 </div>
