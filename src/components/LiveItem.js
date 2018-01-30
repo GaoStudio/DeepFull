de2ec271766543a2b839a19080894259747dbbd7
gaowei <gaowei@tidebuy.net>
2018-01-30 17:55:55 +0800

    add musicplayer

--- src/components/LiveItem.js
+++ src/components/LiveItem.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
 import './components.css';
+import MusicPlayer from "./MusicPlayer";
 class LiveItem extends Component {
     render(){
         return(
             <div className="liveItemContainer">
                 {this.props.data.content}
+                <MusicPlayer />
+                <div className="liveItemfootbar">
+                    <span className="liveItemfootbar-time">30 April 2015, Sunday</span>
+                    <span className="liveItemfootbar-address">Beijing</span>
+                </div>
+                <div  className="liveItemMusic">
+
+                </div>
             </div>
         )
     }
---
+++ src/components/MusicPlayer.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import './MusicPlayer.css';
+class MusicPlayer extends Component {
+    render() {
+        return (
+            <div className="MusicPlayer">
+                <div className="MusicPlayerWrapper">
+                    <img className="MusicPlayerImg" src={require('../images/cover.png')} />
+                    <div className="MusicPlayerControl">
+                        <div className="MusicPlayerControlWrapper">
+                            <div className="MusicPlay">
+                                <div className="MusicPlayButton">
+                                    <button type="button" aria-controls="mep_0" title="Play/Pause"></button>
+                                </div>
+                                <div className="MusicPlayerName">等你下课 -- 周杰伦</div>
+                            </div>
+                            <div className="SoundPlay">
+                                <div className="MusicPlayButton">
+                                    <button type="button" aria-controls="mep_0" title="Play/Pause"></button>
+                                </div>
+                                <div className="SoundPlaySlider">
+                                    <div className="total"></div>
+                                    <div className="current"></div>
+                                    <div className="handle"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="MusicPlayerProgress">
+                    <div className="loaded"></div>
+                    <div className="current"></div>
+                    <img className="control" src={require('../images/handle.png')}></img>
+                    <span className="float">
+                        <span className="float-current">00:16</span>
+                        <span className="float-corner"></span>
+                    </span>
+                </div>
+            </div>
+        )
+    }
+}
+export default MusicPlayer
