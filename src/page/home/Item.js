d8d851217d58c1f4a5d23691443de567611e15d7
gaowei <gaowei@tidebuy.net>
2018-02-01 18:13:01 +0800

    link server

--- src/page/home/Item.js
+++ src/page/home/Item.js
@@ -10,22 +10,22 @@ class Item extends Component {
     render() {
         return(
             <div className="post-list-item">
-                <Link className='post-link' to={{pathname:`${this.props.location.pathname}/5734857`}}>
+                <Link className='post-link' to={{pathname:`${this.props.location.pathname}/${this.props.data.bblog_id}`}}>
                     <div className="post-list-content">
                         <img style={{width:'100%'}} src={require('../../images/glass.gif')}></img>
-                        <div className="post-list-title">Hello World</div>
-                        <div className="post-list-simple">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam animi consequatur corporis delectus dolorem, excepturi exercitationem id itaque labore laborum molestias nisi omnis quam quod rem sequi sunt, ut.</div>
+                        <div className="post-list-title">{this.props.data.bblog_title}</div>
+                        <div className="post-list-simple">{this.props.data.bblog_sub_title}</div>
                     </div>
                 </Link>
                 <div className="post-list-operation">
                     <div className="post-list-operation-item">
-                        <span className="post-list-time">2018-01-01</span>
+                        <span className="post-list-time">{this.props.data.bblog_time}</span>
                     </div>
                     <div className="post-list-operation-item">
-                        <span className="post-list-views">1314views</span>
+                        <span className="post-list-views">{this.props.data.bblog_views}views</span>
                     </div>
                     <div className="post-list-operation-item">
-                        <span className="post-list-category">java</span>
+                        <span className="post-list-category">{this.props.data.blogCategory.bCategory_name}</span>
                     </div>
                 </div>
             </div>
--- src/page/home/Post.js
+++ src/page/home/Post.js
@@ -4,11 +4,50 @@
 import React, {Component} from 'react';
 import Item from './Item.js'
 import './Post.css';
+import {GET} from "../../utils/request";
+import NProgress from 'nprogress'
 class Post extends Component {
+    constructor(props){
+        super(props)
+        this.state= {
+            blogList: [],
+        }
+    }
+    componentDidMount(){
+        this.loadData();
+    }
+    componentWillReceiveProps(nextProps){
+        if(nextProps.location&&nextProps.location.state){
+            this.loadData();
+        }
+    }
+    loadData=()=>{
+        NProgress.start()
+        console.log(this.props)
+        let path = this.props.location.state?this.props.location.state.bCategory_path:this.props.match.params.path;
+        console.log(path)
+        GET("blog/blogs?path="+path,(data)=>{
+            if(data&&data.status===0){
+                NProgress.done()
+                this.setState({
+                    blogList:data.data,
+                })
+            }else {
+                NProgress.done()
+            }
+        },(err)=>{
+            NProgress.done()
+        })
+    }
     render(){
         return(
             <div>
-                <Item location={this.props.location}/>
+                {
+                    this.state.blogList.map((item)=>{
+                        return <Item key={item.bblog_id} data={item} location={this.props.location}/>
+                    })
+                }
+
             </div>
         )
     }
