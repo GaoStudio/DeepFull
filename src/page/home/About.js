a98867f1ed2165aeaf9d3c88af40ac08aa0fdf0e
gaowei <810915983@qq.com>
2018-11-11 20:01:14 +0800

    修改早起模块

--- src/page/home/About.js
+++ src/page/home/About.js
@@ -6,7 +6,15 @@ import CalendarHeatmap from 'react-calendar-heatmap';
 import 'react-calendar-heatmap/dist/styles.css';
 import ReactTooltip from 'react-tooltip';
 import './Article.css';
+import {GET} from "../../utils/request";
 class About extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            morning:null
+        }
+    }
+
     getTooltipDataAttrs = (value) => {
         // Temporary hack around null value.date issue
         if (!value || !value.date) {
@@ -14,9 +22,51 @@ class About extends Component {
         }
         // Configuration for react-tooltip
         return {
-            'data-tip': `${value.date} Get up at ${value.time}`,
+            'data-tip': `${value.date} Get up at ${value.getup_time}`,
         };
     };
+    componentDidMount(){
+        this.loadData();
+    }
+    loadData(){
+        GET("api/user/getupdata",(data)=>{
+            if(data&&data.status===0){
+                this.setState({
+                    morning:data.data,
+                })
+            }
+        },(err)=>{
+           console.log(err)
+        })
+    }
+    renderMorning=()=>{
+        return(
+            <div className="article">
+                <div className="articleTitle">
+                    <div className="articleName">Good Morning</div>
+                    <div className="articleDetailLine"></div>
+                </div>
+                <div className="articleContent" style={{marginLeft:0,marginRight:20,paddingBottom:0, fontSize:18}}>
+                    <CalendarHeatmap
+                        startDate={new Date(this.state.morning.startDay)}
+                        endDate={new Date(this.state.morning.endDay)}
+                        values={this.state.morning.getUps}
+                        showWeekdayLabels={true}
+                        tooltipDataAttrs={this.getTooltipDataAttrs}
+                        gutterSize={1}
+                        weekdayLabels={['柒','壹','贰','叁','肆','伍','陆']}
+                        classForValue={(value) => {
+                            if (!value) {
+                                return 'color-empty';
+                            }
+                            return `color-scale-${value.getup_level}`;
+                        }}
+                    />
+                </div>
+                <ReactTooltip />
+            </div>
+        )
+    }
     render(){
         return(
             <div>
@@ -37,30 +87,8 @@ class About extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="article">
-                    <div className="articleTitle">
-                        <div className="articleName">Good Morning</div>
-                        <div className="articleDetailLine"></div>
-                    </div>
-                    <div className="articleContent" style={{marginLeft:0,marginRight:20,paddingBottom:0, fontSize:18}}>
-                        <CalendarHeatmap
-                            startDate={new Date('2017-12-31')}
-                            endDate={new Date('2018-11-11')}
-                            values={[]}
-                            showWeekdayLabels={true}
-                            tooltipDataAttrs={this.getTooltipDataAttrs}
-                            gutterSize={1}
-                            weekdayLabels={['壹','贰','叁','肆','伍','陆','柒']}
-                            classForValue={(value) => {
-                                if (!value) {
-                                    return 'color-empty';
-                                }
-                                return `color-scale-${value.count}`;
-                            }}
-                        />
-                    </div>
-                </div>
-                <ReactTooltip />
+                {this.state.morning?this.renderMorning():null}
+
             </div>
         )
     }
